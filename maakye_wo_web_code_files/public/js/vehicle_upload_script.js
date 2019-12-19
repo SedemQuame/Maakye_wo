dcae6cae9b3b25b768ffaec412603759cc7c559d
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-12-19 18:05:07 +0000

    worked on page structure, except for admin's

--- maakye_wo_web_code_files/public/js/vehicle_upload_script.js
+++ maakye_wo_web_code_files/public/js/vehicle_upload_script.js
@@ -7,9 +7,9 @@ updateList = function() {
     const input = document.getElementById('vehicle_images');
     const output = document.getElementById('fileList');
 
-    output.innerHTML = '<ul class="list-unstyled">';
+    output.innerHTML = '<ul class="uploaded_vehicle_img">';
     for (let i = 0; i < input.files.length; ++i) {
-        output.innerHTML += '<li>' + input.files.item(i).name + '</li>';
+        output.innerHTML += '<li class="uploaded_vehicle_img_list_item">' + input.files.item(i).name + '</li>';
     }
     output.innerHTML += '</ul>';
 };
