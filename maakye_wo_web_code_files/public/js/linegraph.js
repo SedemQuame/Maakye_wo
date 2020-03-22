a11bffc8dec7c11d475ed628d61b3ebcb92c31f5
SedemQuame <sedem.amekpewu.3@gmail.com>
2020-03-22 00:25:39 +0000

    working UI for violator analyser page.

--- maakye_wo_web_code_files/public/js/linegraph.js
+++ maakye_wo_web_code_files/public/js/linegraph.js
@@ -1,35 +1,98 @@
-$(function () {
-    var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+
+// jshint esversion:6
+const lineChartCanvas = document.getElementById("lineChart");
+
+// creating new linechart
+let lineChart = new Chart(lineChartCanvas, {
+    // type of chart to create
+    type: "line",
+    // data to use, when rendering the chart
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(83, 82, 237, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+    // configuration options go here
+    options: {}
+});
+
+
+const barChartCanvas = document.getElementById("barChart");
+
+// creating new barChart
+let barChart = new Chart(barChartCanvas, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
+    }
+});
+
+const doughChart = document.getElementById("doughNutChart");
+
+const doughNutChart = new Chart(doughChart, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ]
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
         ]
-    };
-   
-    var option = {
-    responsive: true,
-    };
-   
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("lineChart").getContext('2d');
-    var myLineChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
+    },
+    options: {}
 });
