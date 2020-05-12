2a30b874dbe08b6d0fc3fb95344b3eb2f079e9db
SedemQuame <sedem.amekpewu.3@gmail.com>
2020-05-12 15:05:30 +0000

    deleted assets from the embedded project files.

--- maakye_wo_web_code_files/routes/videos.routes.js
+++
@@ -1,8 +0,0 @@
-// jshint esversion:6
-// ================================ creating list application route ===================================//
-module.exports = app => {
-    const video = require('./../controllers/videos.controller');
-
-    //========================================== app dashboard routes ============================================//
-    app.route('/getAllViolators').get(video.getAllPossileViolators);
-};
